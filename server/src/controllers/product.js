6a485cadae74311819147bcebec2065d48015298
Hung Lai <hung.lt205155@sis.hust.edu.vn>
2023-06-24 17:34:55 +0700

    commit thieuo server

--- server/src/controllers/product.js
+++ server/src/controllers/product.js
@@ -1,48 +1,57 @@
 const productService = require("../services/product");
 
-exports.getAllProducts = async (req, res) => {
-    try {
-        const products = await productService.getAllProducts();
-        res.json({ data: products, status: "success" });
-    } catch (err) {
-        res.status(500).json({ error: err.message });
-    }
+getAllProducts = async (req, res) => {
+  try {
+    const products = await productService.getAllProducts();
+    res.json({ data: products, status: "success" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };
 
-exports.addProduct = async (req, res) => {
-    try {
-        console.log(req.body);
-        const product = await productService.addProduct(req.body);
-        res.json({ data: product, status: "success" });
-    } catch (err) {
-        res.status(500).json({ error: err.message });
-    }
+addProduct = async (req, res) => {
+  try {
+    console.log(req.file);
+    console.log(req.body);
+    // const product = await productService.addProduct(req.body);
+    // res.json({ data: product, status: "success" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };
 
-exports.getProductById = async (req, res) => {
-    try {
-        const product = await productService.getProductById(req.params.id);
-        res.json({ data: product, status: "success" });
-    } catch (err) {
-        res.status(500).json({ error: err.message });
-    }
+getProductById = async (req, res) => {
+  try {
+    const product = await productService.getProductById(req.params.id);
+    res.json({ data: product, status: "success" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };
 
-exports.updateProduct = async (req, res) => {
-    try {
-        const product = await productService.updateProduct(req.params.id, req.body);
-        res.json({ data: product, status: "success" });
-    } catch (err) {
-        res.status(500).json({ error: err.message });
-    }
+updateProduct = async (req, res) => {
+  try {
+    const product = await productService.updateProduct(req.params.id, req.body);
+    res.json({ data: product, status: "success" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };
 
-exports.deleteProduct = async (req, res) => {
-    try {
-        console.log(req.params.id);
-        const product = await productService.deleteProduct(req.params.id);
-        res.json({ data: product, status: "success" });
-    } catch (err) {
-        res.status(500).json({ error: err.message });
-    }
+deleteProduct = async (req, res) => {
+  try {
+    console.log(req.params.id);
+    const product = await productService.deleteProduct(req.params.id);
+    res.json({ data: product, status: "success" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+module.exports = {
+  getAllProducts,
+  addProduct,
+  getProductById,
+  updateProduct,
+  deleteProduct,
 };
--- server/src/routes/adminProductManage.js
+++ server/src/routes/adminProductManage.js
@@ -1,18 +1,61 @@
-const router = require('express').Router();
-const Authorize = require('../middleware/authorize');
+const router = require("express").Router();
+const Authorize = require("../middleware/authorize");
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./uploads/images");
+  },
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      Date.now() +
+        "-" +
+        Math.round(Math.random() * 1e9) +
+        "." +
+        file.mimetype.split("/")[1]
+    );
+  },
+  encoding: "7bit",
+});
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 5,
+  },
+  fileFilter: (req, file, cb) => {
+    // reject a file
+    if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+      cb(null, true);
+    } else {
+      cb(null, false);
+    }
+  },
+});
 
 const {
-    getAllProducts,
-    addProduct,
-    getProductById,
-    updateProduct,
-    deleteProduct
+  getAllProducts,
+  addProduct,
+  getProductById,
+  updateProduct,
+  deleteProduct,
 } = require("../controllers/product");
 
 //sử dụng midelware để xác thực quyền truy cập của admin.
 router.use(Authorize);
 
-router.route("/").get(getAllProducts).post(addProduct);
-router.route("/:id").get(getProductById).put(updateProduct).delete(deleteProduct);
+router
+  .route("/")
+  .get(getAllProducts)
+  .post(upload.single("image"), (req, res) => {
+    console.log(req.file);
+    console.log(JSON.parse(req.body.variations));
+    res.json({ message: "create succesfully!!!" });
+  });
+router
+  .route("/:id")
+  .get(getProductById)
+  .put(updateProduct)
+  .delete(deleteProduct);
 
-module.exports = router
+module.exports = router;
